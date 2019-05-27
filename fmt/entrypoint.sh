d0e37bd7f5cf9236b229566e10840033649ab0cd
Luke Kysow <1034429+lkysow@users.noreply.github.com>
2019-05-27 13:54:35 -0500

    Handle parse error during fmt
    If there was is an error while parsing the terraform files, fmt
    won't return a list of unformatted files but will just print an error.
    Previously this would cause the action to print a malformed comment. Now
    we detect if parsing failed and just print the parse error.

--- fmt/entrypoint.sh
+++ fmt/entrypoint.sh
@@ -3,9 +3,9 @@ set -e
 cd "${TF_ACTION_WORKING_DIR:-.}"
 
 set +e
-UNFMT_FILES=$(sh -c "terraform fmt -check=true -write=false $*" 2>&1)
+OUTPUT=$(sh -c "terraform fmt -no-color -check -list -recursive $*" 2>&1)
 SUCCESS=$?
-echo "$UNFMT_FILES"
+echo "$OUTPUT"
 set -e
 
 if [ $SUCCESS -eq 0 ]; then
@@ -16,11 +16,22 @@ if [ "$TF_ACTION_COMMENT" = "1" ] || [ "$TF_ACTION_COMMENT" = "false" ]; then
     exit $SUCCESS
 fi
 
-# Iterate through each unformatted file and build up a comment.
-FMT_OUTPUT=""
-for file in $UNFMT_FILES; do
-FILE_DIFF=$(terraform fmt -write=false -diff=true "$file" | sed -n '/@@.*/,//{/@@.*/d;p}')
-FMT_OUTPUT="$FMT_OUTPUT
+if [ $SUCCESS -eq 2 ]; then
+    # If it exits with 2, then there was a parse error and the command won't have
+    # printed out the files that have failed. In this case we comment back with the
+    # whole parse error.
+    COMMENT="\`\`\`
+$OUTPUT
+\`\`\`
+"
+else
+    # Otherwise the output will contain a list of unformatted filenames.
+    # Iterate through each file and build up a comment containing the diff
+    # of each file.
+    COMMENT=""
+    for file in $OUTPUT; do
+        FILE_DIFF=$(terraform fmt -no-color -write=false -diff "$file" | sed -n '/@@.*/,//{/@@.*/d;p}')
+        COMMENT="$COMMENT
 <details><summary><code>$file</code></summary>
 
 \`\`\`diff
@@ -28,15 +39,15 @@ $FILE_DIFF
 \`\`\`
 </details>
 "
-done
+    done
+fi
 
-COMMENT="#### \`terraform fmt\` Failed
-$FMT_OUTPUT
+COMMENT_WRAPPER="#### \`terraform fmt\` Failed
+$COMMENT
 *Workflow: \`$GITHUB_WORKFLOW\`, Action: \`$GITHUB_ACTION\`*
 "
-PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT" '.body = $body')
+PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT_WRAPPER" '.body = $body')
 COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
 curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
 
 exit $SUCCESS
-
