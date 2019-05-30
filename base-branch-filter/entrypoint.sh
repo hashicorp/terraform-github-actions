7a6abeebe6b9691646a75f9da6f3e5737b33ee6b
Tim Jones <timniverse@gmail.com>
2019-05-30 19:17:35 +0200

    Fix base branch filter script

--- base-branch-filter/entrypoint.sh
+++ base-branch-filter/entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 regex="$*"
 base_branch=$(jq -r .pull_request.base.ref "$GITHUB_EVENT_PATH")
 
-if "$actual" | grep -q "$regex"; then
+echo "$base_branch" | grep -Eq "$regex" || {
   echo "base branch \"$base_branch\" does not match \"$regex\""
   exit 78
-fi
+}
