c9d92d276b057a37aea98afde3d603ed274c8dad
Nick Doyle <nick.doyle@intelematics.com>
2019-09-11 10:16:04 +1000

    feat: only curl to COMMENTS_URL if it exists/was set in the event payload

--- fmt/entrypoint.sh
+++ fmt/entrypoint.sh
@@ -48,6 +48,6 @@ $COMMENT
 "
 PAYLOAD=$(echo '{}' | jq --arg body "$COMMENT_WRAPPER" '.body = $body')
 COMMENTS_URL=$(cat /github/workflow/event.json | jq -r .pull_request.comments_url)
-curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null
+if [ "$COMMENTS_URL" != 'null' ] ; then curl -s -S -H "Authorization: token $GITHUB_TOKEN" --header "Content-Type: application/json" --data "$PAYLOAD" "$COMMENTS_URL" > /dev/null ; fi
 
 exit $SUCCESS
