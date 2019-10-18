c25a9e90ee04fad25711a17ee785c2c62fcf09e9
Matthew Sanabria <24284972+sudomateo@users.noreply.github.com>
2019-10-18 16:31:21 -0400

    Adding comments to fmt, init, and validate

--- src/terraform_init.sh
+++ src/terraform_init.sh
@@ -1,21 +1,35 @@
 #!/bin/bash
 
 function terraformInit {
-  echo "init: initializing Terraform configuration in ${tfWorkingDir}"
+  echo "init: info: initializing Terraform configuration in ${tfWorkingDir}"
   initOutput=$(terraform init -input=false)
   initExitCode=${?}
   
   # Exit code of 0 indicates success.
   if [ ${initExitCode} -eq 0 ]; then
-    echo "init: successfully initialized Terraform configuration in ${tfWorkingDir}"
+    echo "init: info: successfully initialized Terraform configuration in ${tfWorkingDir}"
     echo "${initOutput}"
     echo
     exit ${initExitCode}
   # Otherwise, print the output and exit.
   else
-    echo "init: failed to initialize Terraform configuration in ${tfWorkingDir}"
+    echo "init: error: failed to initialize Terraform configuration in ${tfWorkingDir}"
     echo "${initOutput}"
     echo
+
+    # Comment on the pull request
+    if [ "$GITHUB_EVENT_NAME" == "pull_request" ] && [ "${tfComment}" == "1" ]; then
+      initCommentWrapper="#### \`terraform init\` Failed
+\`\`\`
+${initOutput}
+\`\`\`
+*Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`*"
+      echo "init: info: creating JSON"
+      initPayload=$(echo '{}' | jq --arg body "${initCommentWrapper}" '.body = $body')
+      initCommentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+      echo "init: info: commenting on the pull request"
+      curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${initPayload}" "${initCommentsURL}" > /dev/null
+    fi
     exit ${initExitCode}
   fi
 }
--- src/terraform_validate.sh
+++ src/terraform_validate.sh
@@ -16,6 +16,19 @@ function terraformValidate {
     echo "validate: failed to validate Terraform configuration in ${tfWorkingDir}"
     echo "${validateOutput}"
     echo
+    # Comment on the pull request
+    if [ "$GITHUB_EVENT_NAME" == "pull_request" ] && [ "${tfComment}" == "1" ]; then
+      validateCommentWrapper="#### \`terraform validate\` Failed
+\`\`\`
+${validateOutput}
+\`\`\`
+*Workflow: \`${GITHUB_WORKFLOW}\`, Action: \`${GITHUB_ACTION}\`*"
+      echo "validate: info: creating JSON"
+      validatePayload=$(echo '{}' | jq --arg body "${validateCommentWrapper}" '.body = $body')
+      validateCommentsURL=$(cat ${GITHUB_EVENT_PATH} | jq -r .pull_request.comments_url)
+      echo "validate: info: commenting on the pull request"
+      curl -s -S -H "Authorization: token ${GITHUB_TOKEN}" --header "Content-Type: application/json" --data "${validatePayload}" "${validateCommentsURL}" > /dev/null
+    fi
     exit ${validateExitCode}
   fi
 }
